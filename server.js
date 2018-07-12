118c8f062bd2049364c1cf95a82eb40c468e8e72
di2mon <shakhov.dima55@gmail.com>
2018-07-12 16:36:17 +0300

    init

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function (req, res) {
-  res.sendfile('view/index.html');
-});
-
-app.get('/index.html', function (req, res) {
   res.sendfile('view/index.html');
 });
 
@@ -25,3 +24,8 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
+app.post("/form", urlencodedParser, function (request, response) {
+  if(!request.body) return response.sendStatus(400);
+  console.log(request.body);
+response.send(`${request.body.userName} - ${request.body.userAge}`);
+});
