11f6dfcf524e77f3467459aa0e3890fc5109c0cc
di2mon <shakhov.dima55@gmail.com>
2018-07-12 18:54:44 +0300

    init

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.use(express.static(__dirname + "/public"));
