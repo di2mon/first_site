e8d9eb5aac567a2e00d26688bba6e7819c45ada6
di2mon <shakhov.dima55@gmail.com>
2018-07-12 15:18:04 +0300

    init

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Meze!');
+  res.sendfile('view/index.html');
+});
+
+app.get('/index.html', function (req, res) {
+  res.sendfile('view/index.html');
+});
+
+app.get('/form.html', function (req, res) {
+  res.sendfile('view/form.html');
+});
+
+app.get('/about.html', function (req, res) {
+  res.sendfile('view/about.html');
+});
+
+app.get('/main.html', function (req, res) {
+  res.sendfile('view/main.html');
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
